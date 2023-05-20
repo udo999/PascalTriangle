d5bc8dadd3296441583c1378057f1d4e37724cd2
Udi <zeljkoudovicic@gmail.com>
2023-05-20 17:19:37 +0200

    Final project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun pascalTriangle(n: Int): List<List<Int>> {
+    val triangle = mutableListOf<List<Int>>()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    for (row in 0 until  n) {
+
+        val currentRow = mutableListOf<Int>()
+
+
+        for (col in 0..row) {
+
+            if (col == 0 || col == row) {
+
+                currentRow.add(1)
+            } else {
+
+                val previousRow = triangle[row - 1]
+                val sum = previousRow[col - 1] + previousRow[col]
+                currentRow.add(sum)
+            }
+        }
+
+        triangle.add(currentRow)
+    }
+
+    return triangle
+}
+
+fun main() {
+
+    val n = 12
+
+    val pascalTriangle = pascalTriangle(n)
+
+    for (row in pascalTriangle) {
+        println(row.joinToString(" "))
+    }
 }
